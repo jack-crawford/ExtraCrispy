03d8c795ab5bf7c9a60297923ec1d7454243a4fe
JackWork <jackcrawford@tenstreets-Mac-mini-2.local>
2015-10-15 16:30:09 -0500

    new changes
    changed extracrispy to crispyengine, all functions that pull comics are
    in there, and it is now called by testing1

--- testing1.php
+++ testing1.php
@@ -7,42 +7,25 @@ echo "<title> extracrispy </title>";
 echo "</br>";
 echo '</div>';
 echo '<body id="body2">';
-?>
-<?php
+
+//include crispyengine functions
+include 'crispyengine.php';
+
 //xkcd code below here:
-function xkcd() {
-$xkcd = file_get_contents("http://www.xkcd.com");
-
-$xstart = strpos($xkcd, '<div id="co') ;
-
-$xfinish = strpos($xkcd, '/div', $xstart);
-$xcomic = $xfinish - $xstart-1;
-
-
-echo $b;
-echo substr($xkcd, $xstart, $xcomic);
-
-}
-
-?>
-<?php
-//functional code here!
-
 $weekday = date('D');
-echo $weekday;
 
 if ($weekday === "Mon") {
-    echo "time for xkcd!";
+    echo "it's monday, time for xkcd!";
     xkcd();
 }
 
 if ($weekday === "Wed") {
-    echo "time for xkcd!";
+    echo "it's wednesday, time for xkcd!";
     xkcd();
 }
 
 if ($weekday === "Fri") {
-    echo "time for xkcd!";
+    echo "it's friday, time for xkcd!";
     xkcd();
 }
 echo '</body>';
