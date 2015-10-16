49a4f3f1bc9db85b8d7327138d938088fafa0221
jcrawford17 <jackcrawford@MacBook-Air-2.local>
2015-10-15 20:59:04 -0500

    fixed date problem
    WOOO

---
+++ EC.php
@@ -0,0 +1,54 @@
+<?php
+//html code below here:
+echo '<link rel="stylesheet" href="ec.css">';
+echo '<div id="body1">';
+echo '<h1 style="text-align: center"> welcome to extracrispy </h1>';
+echo "<title> extracrispy </title>";
+echo "</br>";
+echo '</div>';
+echo '<body id="body2">';
+date_default_timezone_set('America/Chicago');
+//include crispyengine functions
+include 'crispyengine.php';
+
+
+//content creation
+function comicreader() {
+    $weekday = date('D');
+if ($weekday === "Mon") {
+    echo "it's monday, time for xkcd!";
+    xkcd();
+}
+
+if ($weekday === "Wed") {
+    echo "it's wednesday, time for xkcd!";
+    xkcd();
+}
+
+if ($weekday === "Fri") {
+    echo "it's friday, time for xkcd!";
+    xkcd();
+}
+
+if ($weekday === "Tue") {
+    echo "it's tuesday, time for NIH!";
+    NIH();
+}
+
+if ($weekday === "Thu") {
+    echo "it's thursday, time for NIH!";
+    NIH();
+}
+}
+
+
+
+//content publishing
+comicreader();
+echo "</br>";
+localcontent();
+
+
+echo '</body>';
+  
+?> 
---
+++ crispyengine.php
@@ -0,0 +1,44 @@
+    
+<?php
+
+//welcome to the crispyengine
+//this is where we store the functions that run EC's webcomic trawling features
+//please wear a hard hat
+
+//this crawls the comic Not Invented Here
+function NIH() {
+$NIH = file_get_contents("http://notinventedhe.re");
+$b = "<br/>";
+$Nstart = strpos($NIH, '<div id="comic-content">') ;
+echo $b;
+$Nfinish = strpos($NIH, '><div id="comic-nav">') ;
+echo substr($NIH, 1507, 228);
+}
+
+//this crawls the comic xkcd
+function xkcd() {
+$xkcd = file_get_contents("http://www.xkcd.com");
+
+$xstart = strpos($xkcd, '<div id="co') ;
+
+$xfinish = strpos($xkcd, '/div', $xstart);
+$xcomic = $xfinish - $xstart-1;
+
+
+echo $b;
+echo substr($xkcd, $xstart, $xcomic);
+
+}
+
+
+
+
+function localcontent(){
+    //enginelog: date formatting cannot have slashes for some reason when calling the img file
+    //resolved by changing date formatting to dots, ie m.d.y.jpg
+    $date = ''.date(m).".".date(d).".".date(y).'';
+    echo "<img src='$date.jpg'>";    
+}
+
+?>
+
