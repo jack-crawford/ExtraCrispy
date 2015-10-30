f9ea1475b10ea15b77ce6d1e096d2513406fc8fd
jcrawford17 <jackcrawford@MacBook-Air-2.local>
2015-10-30 11:38:50 -0500

    little stuff

--- x engine - unstable/testpost.php
+++ x engine - unstable/testpost.php
@@ -11,7 +11,7 @@ $openpost = fopen("$date.txt", w);
 $postcontent = "Title: $title \nDate: $date\n Info: $info \nEND";
 
 fwrite($openpost, $postcontent);
-
+echo 'thank you, your post has been recorded and will be published on the date you chose';
 //echo "<img src=$image>" ;
 
 
