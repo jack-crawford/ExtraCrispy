50ec0db16e76b77a570b1adf6357f9c0a3aec197
jcrawford17 <jackcrawford@MacBook-Air-2.local>
2015-11-17 19:50:42 -0600

    side project

---
+++ boredinclass.php
@@ -0,0 +1,11 @@
+<?php
+include "cheatsheat.php";
+echo "Force Calculator Because Why Not.";
+$mass = 85;
+$acceleration = 3;
+$force = $mass * $acceleration;
+echo $force;
+echo $b;
+echo 9.8 * 85;
+
+?>
